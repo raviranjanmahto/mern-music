9872af154a38ce3a435c43e0895ac3c9debbc852
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-07-08 13:16:16 +0530

    navbar and sidebar route config added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AddSong from "./pages/AddSong";
+import AddAlbum from "./pages/AddAlbum";
+import ListSong from "./pages/ListSong";
+import ListAlbum from "./pages/ListAlbum";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className='bg-orange-300 h-screen'>App</div>;
+  return (
+    <div className='flex min-h-screen'>
+      <ToastContainer />
+      <Sidebar />
+      <div className='flex-1 h-screen bg-[#F3FFF7]'>
+        <Navbar />
+        <div className='pt-8 pl-5 sm:pt-12'>
+          <Routes>
+            <Route path='/add-song' element={<AddSong />} />
+            <Route path='/add-album' element={<AddAlbum />} />
+            <Route path='/list-song' element={<ListSong />} />
+            <Route path='/list-album' element={<ListAlbum />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,54 @@
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
+      <img
+        className='mt-5 w-[max(10vw,100px)] hidden sm:block'
+        src={assets.logo}
+        alt='logo'
+      />
+      <img
+        src={assets.logo_small}
+        className='mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block'
+        alt=''
+      />
+      <div className='flex flex-col mt-10 gap-5'>
+        <NavLink
+          to='/add-song'
+          className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'
+        >
+          <img src={assets.add_song} className='w-5' alt='song icon' />
+          <p className='hidden sm:block'>Add Song</p>
+        </NavLink>
+
+        <NavLink
+          to='/list-song'
+          className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'
+        >
+          <img src={assets.song_icon} className='w-5' alt='song icon' />
+          <p className='hidden sm:block'>List Song</p>
+        </NavLink>
+
+        <NavLink
+          to='/add-album'
+          className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'
+        >
+          <img src={assets.add_album} className='w-5' alt='album icon' />
+          <p className='hidden sm:block'>Add Album</p>
+        </NavLink>
+
+        <NavLink
+          to='/list-album'
+          className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'
+        >
+          <img src={assets.album_icon} className='w-5' alt='album icon' />
+          <p className='hidden sm:block'>List Album</p>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
