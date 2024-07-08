a9f6cce61c4369dd0cc7f18561d5cd0327038e59
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-07-08 20:47:35 +0530

    dynamic album data

--- admin/src/pages/AddSong.jsx
+++ admin/src/pages/AddSong.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -10,6 +10,7 @@ const AddSong = () => {
   const [desc, setDesc] = useState("");
   const [album, setAlbum] = useState("none");
   const [loading, setLoading] = useState(false);
+  const [albumData, setAlbumData] = useState([]);
 
   const URL = import.meta.env.VITE_APP_BACKEND_URL;
 
@@ -39,6 +40,19 @@ const AddSong = () => {
     setLoading(false);
   };
 
+  useEffect(() => {
+    const loadAlbumData = async () => {
+      try {
+        const response = await axios(`${URL}/api/v1/album/list`);
+        if (response.status === 200) setAlbumData(response.data.albums);
+      } catch (error) {
+        toast.error("Failed to load albums. Please try again.");
+      }
+    };
+
+    loadAlbumData();
+  }, [URL]);
+
   if (loading)
     return (
       <div className='grid place-items-center min-h-[80vh]'>
@@ -53,13 +67,14 @@ const AddSong = () => {
     >
       <div className='flex gap-8'>
         <div className='flex flex-col gap-4'>
-          <p>Upload song</p>
+          <p>Upload song*</p>
           <input
             onChange={e => setAudio(e.target.files[0])}
             type='file'
             id='song'
             accept='audio/*'
             hidden
+            required
           />
           <label htmlFor='song'>
             <img
@@ -70,13 +85,14 @@ const AddSong = () => {
           </label>
         </div>
         <div className='flex flex-col gap-4'>
-          <p>Upload image</p>
+          <p>Upload image*</p>
           <input
             onChange={e => setImage(e.target.files[0])}
             type='file'
             id='image'
             accept='image/*'
             hidden
+            required
           />
           <label htmlFor='image'>
             <img
@@ -91,7 +107,7 @@ const AddSong = () => {
       </div>
 
       <div className='flex flex-col gap-2.5'>
-        <p>Song name</p>
+        <p>Song name*</p>
         <input
           onChange={e => setName(e.target.value)}
           value={name}
@@ -103,7 +119,7 @@ const AddSong = () => {
       </div>
 
       <div className='flex flex-col gap-2.5'>
-        <p>Song description</p>
+        <p>Song description*</p>
         <input
           onChange={e => setDesc(e.target.value)}
           value={desc}
@@ -115,13 +131,19 @@ const AddSong = () => {
       </div>
 
       <div className='flex flex-col gap-2.5'>
-        <p>Album</p>
+        <p>Album*</p>
         <select
           onChange={e => setAlbum(e.target.value)}
           defaultValue={album}
           className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]'
+          required
         >
-          <option value='none'>None</option>
+          <option value=''>Select from below</option>
+          {albumData?.map((item, index) => (
+            <option key={index} value={item.name}>
+              {item.name}
+            </option>
+          ))}
         </select>
       </div>
 
